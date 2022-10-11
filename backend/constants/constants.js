0337b71718c1b7ee2def77242192306598a02338
Mary Snegova <Marya0@yandex.ru>
2022-10-11 20:55:27 +0300

    fix: добавить разрешенные домены

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -1,6 +1,10 @@
 const SALT_OR_ROUNDS = 10;
 const regex = /(https?:\/\/)([www.]?[a-zA-Z0-9-]+\.)([^\s]{2,})/;
-const allowedCors = ['http://mesto.mmsnegova.nomoredomains.icu']
+const allowedCors = [
+  'https://mesto.mmsnegova.nomoredomains.icu',
+  'http://mesto.mmsnegova.nomoredomains.icu',
+  'http://localhost:3000',
+];
 
 module.exports = {
   SALT_OR_ROUNDS,
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://api.mesto.mmsnegova.nomoredomains.icu/#/";
+export const BASE_URL = "http://api.mesto.mmsnegova.nomoredomains.icu";
 
 export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
