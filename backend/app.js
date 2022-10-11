2cf205b81c38b39ba4914a02b675b712e4bcf7b6
Mary Snegova <Marya0@yandex.ru>
2022-10-11 22:32:12 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    /* res.header('Access-Control-Allow-Credentials', true); */
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
@@ -42,8 +42,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
-
-
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
