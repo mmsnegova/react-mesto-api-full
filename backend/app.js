239961ed9191822e9aa0e53ae7e6224afbfba04c
Mary Snegova <Marya0@yandex.ru>
2022-10-11 22:21:56 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
-const cors = require('cors');
+/* const cors = require('cors'); */
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const {
@@ -18,7 +18,7 @@ const { regex, allowedCors } = require('./constants/constants');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use('*', cors());
+/* app.use('*', cors()); */
 
 /* app.use((req, res, next) => {
   const { origin } = req.headers;
